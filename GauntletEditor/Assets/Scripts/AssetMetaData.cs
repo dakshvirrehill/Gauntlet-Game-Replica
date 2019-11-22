ab986f869dcec110a3428ba986cb1ab0814e94a0
dakshvir_rehill <dakshvirr@gmail.com>
2019-11-22 16:51:12 -0500

    Added empty scriptable objects and complete Asset Meta Data scriptable object

---
+++ GauntletEditor/Assets/Scripts/AssetMetaData.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetMetaData : ScriptableObject
+{
+    public enum AssetType
+    {
+        TextureAsset,
+        AudioAsset,
+        FontAsset,
+        PrefabAsset
+    }
+    public string mAssetFilePath;
+    public string mGUID;
+}
