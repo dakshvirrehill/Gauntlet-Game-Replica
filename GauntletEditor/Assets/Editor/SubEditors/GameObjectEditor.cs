a4dfe429b76648453a004fd3d4ff7f834e9fc84d
dakshvir_rehill <dakshvirr@gmail.com>
2019-11-23 16:35:39 -0500

    GameObject main editor completed

--- GauntletEditor/Assets/Editor/SubEditors/GameObjectEditor.cs
+++ GauntletEditor/Assets/Editor/SubEditors/GameObjectEditor.cs
@@ -10,6 +10,7 @@ public class GameObjectEditor
         None,
         Projectile,
         Pickable,
+        Enemy,
         SpawnFactory,
         StaticObject,
     }
@@ -18,6 +19,10 @@ public class GameObjectEditor
     GameObjectType mActiveType = GameObjectType.None;
     GameScriptable mScriptable;
     EnumField mTypeEnum;
+    ObjectField mSelectionField;
+
+    VisualElement mCurrentObjectElement;
+
     static void CreateInstance()
     {
         if(mInstance == null)
@@ -34,7 +39,76 @@ public class GameObjectEditor
         mInstance.mTypeEnum = mInstance.mGameObjectEditorUI.Q<EnumField>("gobj_type");
         mInstance.mTypeEnum.Init(GameObjectType.None);
         mInstance.mTypeEnum.RegisterCallback<ChangeEvent<GameObjectType>>((aEv) => mInstance.OnTypeChanged(aEv.newValue));
+        mInstance.mSelectionField = mInstance.mGameObjectEditorUI.Q<ObjectField>("scriptable_gobj_field");
+        mInstance.mSelectionField.RegisterCallback<ChangeEvent<Object>>((aEv) => mInstance.OnSelectionChanged(aEv.newValue));
         return mInstance.mGameObjectEditorUI;
+    }
+
+    void SetObjectFieldType()
+    {
+        mSelectionField.SetValueWithoutNotify(null);
+        mSelectionField.SetEnabled(true);
+        switch (mActiveType)
+        {
+            case GameObjectType.None:
+                mSelectionField.SetEnabled(false);
+                break;
+            case GameObjectType.Pickable:
+                mSelectionField.objectType = typeof(Item);
+                break;
+            case GameObjectType.Projectile:
+                mSelectionField.objectType = typeof(Projectile);
+                break;
+            case GameObjectType.SpawnFactory:
+                mSelectionField.objectType = typeof(SpawnFactory);
+                break;
+            case GameObjectType.StaticObject:
+                mSelectionField.objectType = typeof(StaticObject);
+                break;
+        }
+    }
+
+    void RemoveCurrentObjectVE()
+    {
+        if(mCurrentObjectElement != null && mGameObjectEditorUI.Contains(mCurrentObjectElement))
+        {
+            mGameObjectEditorUI.Remove(mCurrentObjectElement);
+        }
+        mCurrentObjectElement = null;
+    }
+
+    void CreateNewObjectVE()
+    {
+        VisualTreeAsset aAsset;
+        switch (mActiveType)
+        {
+            case GameObjectType.None:
+                return;
+            case GameObjectType.Pickable:
+                aAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UXML Files/PickableEditor.uxml");
+                break;
+            case GameObjectType.Projectile:
+                aAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UXML Files/ProjectileEditor.uxml");
+                break;
+            case GameObjectType.SpawnFactory:
+                aAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UXML Files/SpawnFactoryEditor.uxml");
+                break;
+            case GameObjectType.Enemy:
+                aAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UXML Files/EnemyEditor.uxml");
+                break;
+            case GameObjectType.StaticObject:
+                aAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UXML Files/StaticObjectEditor.uxml");
+                break;
+            default:
+                return;
+        }
+        if(aAsset == null)
+        {
+            return;
+        }
+        mCurrentObjectElement = aAsset.CloneTree();
+        mCurrentObjectElement.Q<Button>("gobj_data").RegisterCallback<MouseUpEvent>((aEv) => SaveAsScriptableAsset());
+        mGameObjectEditorUI.Add(mCurrentObjectElement);
     }
 
     void OnTypeChanged(GameObjectType pNewType)
@@ -44,7 +118,21 @@ public class GameObjectEditor
             mTypeEnum.value = mActiveType;
             return;
         }
+        RemoveCurrentObjectVE();
+        mActiveType = pNewType;
+        SetObjectFieldType();
+        CreateNewObjectVE();
+    }
 
+    void OnSelectionChanged(Object pSelectedObject)
+    {
+        //setup values according to the object type
+    }
+
+    void SaveAsScriptableAsset()
+    {
+        Debug.Log("Save Scriptable Asset Function Called");
+        mTypeEnum.value = GameObjectType.None;
     }
 
 }
