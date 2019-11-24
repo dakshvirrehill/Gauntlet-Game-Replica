215eff9721999a33f1cd17a24fdd51b7e7b4cfc2
Dakshvir Rehill <dakshvirr@gmail.com>
2019-11-24 14:19:01 -0500

    Created basic level editor functionality

--- GauntletEditor/Assets/Editor/SubEditors/LevelEditor.cs
+++ GauntletEditor/Assets/Editor/SubEditors/LevelEditor.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEditor.UIElements;
 
-public class LevelEditor
+public class LevelEditor : IBindable
 {
     static LevelEditor mInstance;
     VisualElement mLevelEditorUI = null;
@@ -16,6 +16,12 @@ public class LevelEditor
     Vector2 mLevelListGUIScrollPos;
     Vector2 mLevelMapGUIScrollPos;
     Vector2 mPlaceableObjectsGUIScrollPos;
+
+    Level mActiveLevel;
+    VisualElement mLevelData;
+    VisualElement mEditorMain;
+    IMGUIContainer mMapContainer;
+    IMGUIContainer mPlaceableObjectContainer;
 
     static void CreateInstance()
     {
@@ -30,13 +36,72 @@ public class LevelEditor
         CreateInstance();
         VisualTreeAsset aLevelEditorAsset = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Assets/Editor/UXML Files/LevelEditor.uxml");
         mInstance.mLevelEditorUI = aLevelEditorAsset.CloneTree();
+        mInstance.SetDataFromUXML();
         return mInstance.mLevelEditorUI;
     }
 
+    void SetDataFromUXML()
+    {
+        mEditorMain = mLevelEditorUI.Q<VisualElement>("editor_data");
+        mLevelData = mEditorMain.Q<VisualElement>("main_data_display");
+        mEditorMain.Remove(mLevelData);
+        mLevelData.Q<ObjectField>("gameplay_music").objectType = typeof(AudioClip);
+        mLevelData.Q<SliderInt>("row_size").RegisterCallback<ChangeEvent<int>>((aEv) => UpdateLevelMapGrid());
+        mLevelData.Q<SliderInt>("column_size").RegisterCallback<ChangeEvent<int>>((aEv) => UpdateLevelMapGrid());
+        mEditorMain.Q<IMGUIContainer>("level_list").onGUIHandler = LevelListOnGUI;
+        mMapContainer = mLevelData.Q<IMGUIContainer>("level_map");
+        mPlaceableObjectContainer = mLevelData.Q<IMGUIContainer>("placeable_objects");
+        mMapContainer.onGUIHandler = LevelMapOnGUI;
+        mMapContainer.RegisterCallback<MouseUpEvent>(ClickGrid);
+        mPlaceableObjectContainer.onGUIHandler = PlaceableObjectListOnGUI;
+        mPlaceableObjectContainer.RegisterCallback<MouseUpEvent>(ClickPlaceable);
+
+        mAllLevels = new List<Level>();
+        mLevelList = new ReorderableList(mAllLevels, typeof(Level));
+        mLevelList.drawHeaderCallback = (Rect aRect) => {
+            EditorGUI.LabelField(aRect, "Current Levels In Order");
+        };
+        mLevelList.drawElementCallback = UpdateLevelList;
+        mLevelList.onAddCallback = AddNewLevel;
+        mLevelList.onCanRemoveCallback = (ReorderableList pList) =>
+        {
+            return pList.count > 1;
+        };
+        mLevelList.onSelectCallback = SelectLevel;
+    }
 
     #region IMGUI
-    #endregion
+    void LevelListOnGUI()
+    {
+        mLevelListGUIScrollPos = EditorGUILayout.BeginScrollView(mLevelListGUIScrollPos, GUILayout.Width(1050), GUILayout.Height(50));
+        mLevelList.DoLayoutList();
+        EditorGUILayout.EndScrollView();
+    }
 
+    void LevelMapOnGUI()
+    {
+        mLevelMapGUIScrollPos = EditorGUILayout.BeginScrollView(mLevelMapGUIScrollPos, GUILayout.Width(600), GUILayout.Height(250));
+        //draw grid
+        EditorGUILayout.EndScrollView();
+    }
+
+    void PlaceableObjectListOnGUI()
+    {
+        mPlaceableObjectsGUIScrollPos = EditorGUILayout.BeginScrollView(mPlaceableObjectsGUIScrollPos, GUILayout.Width(600), GUILayout.Height(250));
+        //draw objects
+        EditorGUILayout.EndScrollView();
+    }
+    #endregion
+    void SetupNewLevel()
+    {
+        if (mEditorMain.Contains(mLevelData))
+        {
+            mEditorMain.Remove(mLevelData);
+        }
+        mLevelData.Bind(new SerializedObject(mActiveLevel));
+        mEditorMain.Add(mLevelData);
+    }
+    #region CallBacks
     void UpdateLevelList(Rect aRect, int aIx, bool aIsActive, bool aIsFocused)
     {
         var aElement = (Level)mLevelList.list[aIx];
@@ -46,9 +111,36 @@ public class LevelEditor
 
     void AddNewLevel(ReorderableList pList)
     {
-        Level aLevel = new Level();
-        mAllLevels.Add(aLevel);
-        //do something
+        mActiveLevel = new Level();
+        mAllLevels.Add(mActiveLevel);
+        SetupNewLevel();
     }
+
+    void SelectLevel(ReorderableList pList)
+    {
+        mActiveLevel = (Level)pList.list[pList.index];
+        SetupNewLevel();
+    }
+
+    void UpdateLevelMapGrid()
+    {
+        //make grid
+    }
+
+    void ClickGrid(MouseUpEvent pEvent)
+    {
+        Debug.Log("Grid Clicked");
+    }
+
+    void ClickPlaceable(MouseUpEvent pEvent)
+    {
+        Debug.Log("Placeable Clicked");
+    }
+
+    #endregion
+
+    public IBinding binding { get; set; }
+    public string bindingPath { get; set; }
+
 
 }
--- GauntletEditor/Assets/Scripts/Level.cs
+++ GauntletEditor/Assets/Scripts/Level.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class Level : GameScriptable
 {
     public string mName;
+    public int mRows;
+    public int mColumns;
+    public float mTime;
     public Level()
     {
         mName = "Untitled Level";
+        mColumns = 32;
+        mRows = 32;
+        mTime = 60;
     }
 }
