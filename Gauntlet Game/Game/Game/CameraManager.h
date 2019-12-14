78fa44ac3acb91667f719c1ce7df5e959bda9c11
Dakshvir Rehill <dakshvirr@gmail.com>
2019-12-14 16:55:04 -0500

    Completed Camera Movement Manager

---
+++ Gauntlet Game/Game/Game/CameraManager.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef _CAMERA_MANAGER_H_
+#define _CAMERA_MANAGER_H_
+#include "Component.h"
+class Player;
+class CameraManager : public Component
+{
+	DECLARE_DYNAMIC_DERIVED_CLASS(CameraManager, Component)
+	Player* mPlayer = nullptr;
+protected:
+	virtual void initialize() override;
+	virtual void load(json::JSON&) override;
+	virtual void update(float deltaTime) override;
+public:
+	void setPlayer(Player* pPlayer) { mPlayer = pPlayer; }
+};
+#endif
